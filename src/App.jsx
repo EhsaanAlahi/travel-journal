1b640b04491f5ac18ec764281bc3f49380cb67e6
Ehsan Alahi <connectwithehsan@gmail.com>
2026-03-19 17:11:56 +0500

    entery ->Japan component html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./component/Header";
+import Entery from "./component/Entery";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Entery/>
       <p>Welcome to my travel journal app!</p>
     </div>
   );
