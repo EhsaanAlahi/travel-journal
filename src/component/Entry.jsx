371932c41265f01d932c1fad921ac91f967231ad
Ehsan Alahi <connectwithehsan@gmail.com>
2026-03-28 23:14:15 +0500

    google maps link updated

--- src/component/Entry.jsx
+++ src/component/Entry.jsx
@@ -12,7 +12,7 @@ export default function Entery(props) {
             <div className="info-container">
                 <img className="marker" src={marker} alt="marker point" />
                 <span className="country">{props.country}</span>
-                <a href={props.googleMapsLinks} target="_blank">View on google map</a>
+                <a href={props.googleMapsLink} target="_blank">View on google map</a>
                 <h2 className="title">{props.title}</h2>
                 <p className="tip-date">{props.date} </p>
                 <p className="trip-discription">{props.description}</p>
